c7380af5104624a27ddaf28d6e8e82ee8a655014
elieelijah <ganzaelie10@gmail.com>
2023-03-21 13:26:33 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,10 +9,10 @@
  * Return: 1 is c if true else 0
 */
 
-int _isalpha(int c)
-{
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
-		return (1);
-	return (0);
+int _isalpha(int c) {
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
