df293c04897cdff2323c5f764788ba835105b845
elieelijah <ganzaelie10@gmail.com>
2023-03-21 10:07:59 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main - cleck the code
+ *
+ * Description: a C program that prints _putchar
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0, ch < 8, ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+
+	return (0);
+
+}
