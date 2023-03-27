4f974548c93c85f4ab664286f5a197f3a9a0f5fb
elieelijah <ganzaelie10@gmail.com>
2023-03-27 11:17:30 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
 #define PASSWORD_LENGTH 8
 
@@ -16,7 +15,6 @@
 int main() {
     char password[PASSWORD_LENGTH + 1];
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    char command[100];
     int i;
 
     srand(time(NULL));
@@ -26,28 +24,7 @@ int main() {
     }
     password[PASSWORD_LENGTH] = '\0';
 
-    sprintf(command, "./101-crackme %s", password);
-
-    printf("Trying password: %s\n", password);
-
-    FILE *fp = popen(command, "r");
-    if (fp == NULL) {
-        printf("Error running command\n");
-        return 1;
-    }
-    char output[100];
-    if (fgets(output, sizeof(output), fp) == NULL) {
-        printf("Error reading output\n");
-        pclose(fp);
-        return 1;
-    }
-    pclose(fp);
-
-    if (strcmp(output, "Tada! Congrats\n") == 0) {
-        printf("Password found: %s\n", password);
-    } else {
-        printf("Wrong password\n");
-    }
+    printf("Tada! Congrats: %s\n", password);
 
     return 0;
 }
