e3786c39521c0eb808bc93938f90a8c0773ad661
elieelijah <ganzaelie10@gmail.com>
2023-04-05 08:16:29 -0700

    main file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -8,5 +8,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
