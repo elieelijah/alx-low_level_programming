af8886300414941407a2b92c39f791ec3533d6c3
elieelijah <ganzaelie10@gmail.com>
2023-04-17 04:08:07 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
